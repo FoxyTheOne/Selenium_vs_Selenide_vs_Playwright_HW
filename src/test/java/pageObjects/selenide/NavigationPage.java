7dcfe2b224c52b8796fcd5f89bbcb3ba6ded0ac3
FoxyTheOne <gartel.av@gmail.com>
2025-05-07 15:00:29 +0300

    Upgrading Navigation page tests

--- src/test/java/pageObjects/selenide/NavigationPage.java
+++ src/test/java/pageObjects/selenide/NavigationPage.java
@@ -1,11 +1,12 @@
 package pageObjects.selenide;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import constants.CommonConstants;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 import static constants.CommonConstants.BASE_URL;
@@ -34,27 +35,74 @@ public class NavigationPage extends BasePage {
         );
     }
 
-    @Step("Check the page button number")
-    public void checkIsItAnExpectedPageNumber(int pageNumber) {
-        String expectedPageNumberString = String.format("%s", pageNumber);
-        // Формируем XPath с явным указанием на текст и активное состояние
-        String activePageXpath = String.format(
-                "//li[contains(@class, 'active')]//a[contains(normalize-space(), '%d')]",
-                pageNumber
-        );
-
-        // Используем $x() для XPath и проверяем элемент
-        SelenideElement activePageNumberButton = $x(activePageXpath)
-                .as("Active page link")
-                .shouldBe(visible)
-                .shouldHave(
-                        attribute("href",
-                                CommonConstants.BASE_URL + "navigation" +
-                                        pageNumber +
-                                        ".html"
-                        )
-                );
-
-        assertEquals(expectedPageNumberString, activePageNumberButton.getText());
+    @Step("Find Previous button element")
+    public SelenideElement getPreviousButton() {
+        return $(".page-link").shouldHave(text("Previous"));
     }
+
+//    @Step("Find Page button element")
+//    public SelenideElement getPageByNumber(int number) {
+//        return $x(String.format("//li[@class='page-item']/a[text()='%d']", number));
+//    }
+
+    @Step("Find Active page button element")
+    public SelenideElement getActivePage() {
+        return $("li.page-item.active a.page-link");
+    }
+
+    @Step("Find Next button element")
+    public SelenideElement getNextButton() {
+//        return $(".page-link").shouldHave(text("Next"));
+
+        ElementsCollection pageLinks = $$("ul.pagination li.page-item");
+
+//        boolean elementFound = false;
+//
+//        for (SelenideElement pageButton : pageLinks) {
+//            try {
+//                pageButton.shouldHave(text("Next"));
+//                elementFound = true;
+//            } catch (InvalidSelectorException | UnhandledAlertException | ElementNotFound | ElementShouldNot |
+//                     ElementShould e) {
+//                System.out.println(e.getMessage());
+//            }
+//
+//            if (elementFound) {
+//                return pageButton;
+//            }
+//        }
+//
+//        return null;
+
+        return pageLinks.last();
+    }
+
+    @Step("Counting number of buttons (pages + next + previous buttons)")
+    public ElementsCollection getPaginationItems() {
+        return $$("ul.pagination li.page-item");
+    }
+
+//    @Step("Check the page button number")
+//    public void checkIsItAnExpectedPageNumber(int pageNumber) {
+//        String expectedPageNumberString = String.format("%s", pageNumber);
+//        // Формируем XPath с явным указанием на текст и активное состояние
+//        String activePageXpath = String.format(
+//                "//li[contains(@class, 'active')]//a[contains(normalize-space(), '%d')]",
+//                pageNumber
+//        );
+//
+//        // Используем $x() для XPath и проверяем элемент
+//        SelenideElement activePageNumberButton = $x(activePageXpath)
+//                .as("Active page link")
+//                .shouldBe(visible)
+//                .shouldHave(
+//                        attribute("href",
+//                                CommonConstants.BASE_URL + "navigation" +
+//                                        pageNumber +
+//                                        ".html"
+//                        )
+//                );
+//
+//        assertEquals(expectedPageNumberString, activePageNumberButton.getText());
+//    }
 }
--- src/test/java/ui/selenidePOMTests/NavigationPageTests.java
+++ src/test/java/ui/selenidePOMTests/NavigationPageTests.java
@@ -1,44 +1,150 @@
 package ui.selenidePOMTests;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import pageObjects.selenide.HomePage;
 import pageObjects.selenide.NavigationPage;
 
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.text;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class NavigationPageTests extends BaseTestSettings {
     HomePage homePage;
+    NavigationPage navigationPage;
 
     @Override
     @BeforeEach
     void setup() {
         super.setup();
         homePage = new HomePage();
+        homePage.open();
+        navigationPage = homePage.openNavigationPage();
+        Configuration.timeout = 5000;
     }
 
     @Step("Open Navigation page test")
     @Test
     void openNavigationTest() throws InterruptedException {
-        homePage.open();
-        NavigationPage navigationPage = homePage.openNavigationPage();
+//        homePage.open();
+//        NavigationPage navigationPage = homePage.openNavigationPage();
 
         navigationPage.checkIsNavigationPage();
     }
 
-    @Step("Click the next button test")
-    @ParameterizedTest
-    @ValueSource(ints = {3, 2, 1})
-    void nextButtonTest(int pageNumber) {
-        homePage.open();
-        NavigationPage navigationPage = homePage.openNavigationPage();
-//        int pageNumber = 3;
+    /**
+     * Проверка поведения по умолчанию при открытии страницы:
+     * Проверяем, что элемент Previous имеет класс 'disabled';
+     * Проверяем, что элемент страницы 1 имеет класс 'active';
+     * Проверяем, что элемент Next не имеет класса 'disabled'.
+     */
+    @Step("Default check: if previous button is disabled, is it page 1 and is next button active")
+    @Test
+    void CheckDefaultBehaviour() {
+        SelenideElement previousButton = navigationPage.getPreviousButton();
+        SelenideElement activePageButton = navigationPage.getActivePage();
+        SelenideElement nextButton = navigationPage.getNextButton();
 
-        for (int i = 1; i < pageNumber; i++) {
-            navigationPage.clickNextButton();
-        }
-
-        navigationPage.checkIsItAnExpectedPageNumber(pageNumber);
+        assertAll(
+                () -> assertDoesNotThrow(() -> {
+                    previousButton.parent().shouldHave(cssClass("disabled"));
+                }),
+                () -> assertDoesNotThrow(() -> {
+                    activePageButton.shouldHave(text("1"));
+                }),
+                () -> assertDoesNotThrow(() -> {
+                    nextButton.shouldNotHave(cssClass("disabled"));
+                })
+        );
     }
+
+    /**
+     * Проверка переходов на другие страницы:
+     * - Кликаем на Next.
+     * - Проверяем, что Previous активен (нет класса 'disabled').
+     * - Проверяем, что текущая страница теперь активна (например, страница 2).
+     * - Кликаем на Previous.
+     * - Проверяем, что Previous снова 'disabled' и активна страница 1.
+     */
+    @Step("Check if previous button is active on the 2 page and is it disabled when we return back on 1 page")
+    @Test
+    void testForwardBackwardNavigation() {
+        // Переход вперед
+        navigationPage.getNextButton().click();
+        Selenide.sleep(500); // Даем время на загрузку
+
+        // Проверяем состояние на второй странице
+        SelenideElement previousButton = navigationPage.getPreviousButton();
+        SelenideElement activePageButton = navigationPage.getActivePage();
+
+        assertAll(
+                () -> assertDoesNotThrow(() -> {
+                    previousButton.parent().shouldNotHave(cssClass("disabled"));
+                }),
+                () -> assertDoesNotThrow(() -> {
+                    activePageButton.shouldHave(text("2"));
+                })
+        );
+
+        // Возврат назад
+        previousButton.click();
+        Selenide.sleep(500);
+
+        assertAll(
+                () -> assertDoesNotThrow(() -> {
+                    previousButton.parent().shouldHave(cssClass("disabled"));
+                }),
+                () -> assertDoesNotThrow(() -> {
+                    activePageButton.shouldHave(text("1"));
+                })
+        );
+    }
+
+    /**
+     * Тест на исправность Next:
+     * - Определяем общее количество страниц.
+     * - Нажимаем Next (количество страниц - 1) раз.
+     * - Проверяем, что Next теперь 'disabled'.
+     */
+    @Step("Check is next button disabled on the last page")
+    @Test
+    void testNextButtonFunctionality() {
+        int totalPages = navigationPage.getPaginationItems().size() - 2;// Исключаем Previous и Next
+        SelenideElement nextButton = navigationPage.getNextButton();
+        SelenideElement activePageButton = navigationPage.getActivePage();
+
+        assertAll(
+                // Проходим все страницы
+                () -> assertDoesNotThrow(() -> {
+                    for (int i = 1; i < totalPages; i++) {
+                        nextButton.click();
+                        Selenide.sleep(500);
+                        activePageButton.shouldHave(text(String.valueOf(i + 1)));
+                    }
+                }),
+                // Проверяем состояние Next на последней странице
+                () -> assertDoesNotThrow(() -> {
+                    nextButton.shouldHave(cssClass("disabled"));
+                })
+        );
+    }
+
+//    @Step("Click the next button test")
+//    @ParameterizedTest
+//    @ValueSource(ints = {3, 2, 1})
+//    void nextButtonTest(int pageNumber) {
+////        homePage.open();
+////        NavigationPage navigationPage = homePage.openNavigationPage();
+//
+//        for (int i = 1; i < pageNumber; i++) {
+//            navigationPage.clickNextButton();
+//        }
+//
+//        navigationPage.checkIsItAnExpectedPageNumber(pageNumber);
+//    }
 }
