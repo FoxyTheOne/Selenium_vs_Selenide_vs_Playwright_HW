0d3fdb5e37154354b43f4b4d8477182c7583612b
FoxyTheOne <gartel.av@gmail.com>
2025-05-12 12:19:15 +0300

    Changing initDriver method

--- src/test/java/ui/selenidePOMTests/BaseTestSettings.java
+++ src/test/java/ui/selenidePOMTests/BaseTestSettings.java
@@ -20,7 +20,7 @@ public class BaseTestSettings {
         if (!config.isRemote()) {
             System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         }
-        initDriver();
+//        initDriver();
     }
 
 //    Selenide Configuration class contains public static MutableCapabilities browserCapabilities which is used by the driver for browser startup, if provided.
@@ -43,30 +43,30 @@ public class BaseTestSettings {
 //            .setProfile(profile);
 //    Configuration.browserCapabilities = options;
 
-    // Selenium code:
-    private void initDriver() {
-        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
-        if (remoteUrl != null) {
-            if (!remoteUrl.isEmpty()) {
-                WebDriver driver;
-                Allure.addAttachment("remote", remoteUrl);
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--headless");  // Add headless mode
-                options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
-                options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
-                options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
-                options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
-                try {
-                    driver = new RemoteWebDriver(new URL(remoteUrl), options);
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
-                }
-                driver.manage().window().maximize();
-            }
-        } else {
-//            Selenide настраивает драйвер по умолчанию, поэтому здесь нам это не нужно
-//            driver = new ChromeDriver();
-        }
-//        driver.manage().window().maximize();
-    }
+//    // Selenium code:
+//    private void initDriver() {
+//        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+//        if (remoteUrl != null) {
+//            if (!remoteUrl.isEmpty()) {
+//                WebDriver driver;
+//                Allure.addAttachment("remote", remoteUrl);
+//                ChromeOptions options = new ChromeOptions();
+//                options.addArguments("--headless");  // Add headless mode
+//                options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
+//                options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
+//                options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
+//                options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+//                try {
+//                    driver = new RemoteWebDriver(new URL(remoteUrl), options);
+//                } catch (MalformedURLException e) {
+//                    throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+//                }
+//                driver.manage().window().maximize();
+//            }
+//        } else {
+////            Selenide настраивает драйвер по умолчанию, поэтому здесь нам это не нужно
+////            driver = new ChromeDriver();
+//        }
+////        driver.manage().window().maximize();
+//    }
 }
